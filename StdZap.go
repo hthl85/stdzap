ee36f79da44811194b2eef27cc86550c63e43175
Le Trong <le.trong>
2020-03-06 11:52:06 -0500

    Initialize stdzap

---
+++ StdZap.go
@@ -0,0 +1,48 @@
+package stdzap
+
+import (
+	zapconf "github.com/hthl85/conf-zap"
+	"go.uber.org/zap"
+)
+
+// StdZap struct
+type StdZap struct {
+	sugar *zap.SugaredLogger
+}
+
+// NewStdZap creates new stdzap
+func NewStdZap(zapConf *zapconf.ZapConf) (std *StdZap, err error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, err
+	}
+
+	if logger != nil {
+		defer func() {
+			if deferErr := logger.Sync(); deferErr != nil {
+				err = deferErr
+			}
+		}()
+	}
+
+	sugar := logger.Sugar()
+
+	return &StdZap{
+		sugar: sugar,
+	}, nil
+}
+
+// Infof logs info
+func (std *StdZap) Infof(msg string, args ...interface{}) {
+	std.sugar.Infof(msg, args)
+}
+
+// Error logs error
+func (std *StdZap) Error(err error) {
+	std.sugar.Error(err)
+}
+
+// Errorf logs error info
+func (std *StdZap) Errorf(msg string, args ...interface{}) {
+	std.sugar.Errorf(msg, args)
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/hthl85/stdzap
+
+go 1.14
+
+require (
+	github.com/hthl85/conf-zap v0.0.0-20200306161833-01eb4cb3fda7 // indirect
+	github.com/hthl85/stdlog v0.0.0-20200306165039-f09c3de3575e // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.14.0 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/tools v0.0.0-20200306161513-8287d625a0c7 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
+)
